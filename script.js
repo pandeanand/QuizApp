0545095581d039cacb44aac66f881de379081984
Anand Pande <110482338+pandeanand@users.noreply.github.com>
2023-02-15 23:39:33 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const quizData = [
+    {
+        question: 'How old is Anand?',
+        a: '22',
+        b: '25',
+        c: '30',
+        d: '20',
+        correct: 'b'
+    },
+    {
+        question: 'How old is Laptop?',
+        a: '2',
+        b: '5',
+        c: '3',
+        d: '0',
+        correct: 'b'
+    },
+    {
+        question: 'Mobile?',
+        a: '8',
+        b: '2',
+        c: '3',
+        d: '6',
+        correct: 'c'
+    }
+]
